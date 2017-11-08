73ba70fe86982d6762b653b6663fbf8fdd727a79
Raimundo Junior <raimundo.colombo@gmail.com>
2017-11-07 23:46:16 -0200

    Criação do Projeto

---
+++ src/main/java/br/com/bsbmob/appentrega/AppEntregaApplication.java
@@ -0,0 +1,12 @@
+package br.com.bsbmob.appentrega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEntregaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEntregaApplication.class, args);
+	}
+}
